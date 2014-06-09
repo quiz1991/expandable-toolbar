d050f274ebae15bbab46d13491361ac07339d7dd
quiz1991 <quizip@gmail.com>
2014-06-09 13:10:19 -0400

    draggable now

--- scripts.js
+++ scripts.js
@@ -2,36 +2,39 @@ $(document).ready(function() {
 	$(".toolbar").click(function(event) {
 		var toolbarList = $(".toolbar-list");
 		// toolbarList.animate({width:'toggle'},350);
-		if(toolbarList.data("direction") === "left") {
+		if(toolbarList.data("status") === "folded") {
+			// alert(toolbarList.data("status"));
 			toolbarList.show().animate({width: "495px"}, {duration: 400});
-			toolbarList.data("direction", "right");
+			toolbarList.data("status", "expanded");
 		}
 		else {
-			toolbarList.show().animate({width: "90px"}, {duration: 400});
-			toolbarList.data("direction", "left");
+			toolbarList.show().animate({width: "90px"}, {duration: 400, complete: function() {toolbarList.hide();}});
+			// alert(toolbarList.data("status"));
+			toolbarList.data("status", "folded");
 		}
 	});
 
-	$(".test").on("dragstart", function(event) {
+	$(".toolbar").on("dragstart", function(event) {
 		var e = event.originalEvent;
-		// console.log(parseInt($(event.target).css("left"), 10));
-    e.dataTransfer.setData("text/plain",
-    (parseInt($(event.target).css("left"), 10) - e.clientX) + ',' + (parseInt($(event.target).css("top"),10) - e.clientY));
-    // console.log(e.dataTransfer.getData("text/plain"));
-	});
-
-	$("body").on("drop", function(event) {
-		alert("haha");
-		var e = event.originalEvent;
-		var offset = e.dataTransfer.getData("text/plain").split(',');
-		console.log(offset);
-    $(".test").css("left", (e.clientX + parseInt(offset[0],10)) + 'px');
-    $(".test").css("top", (e.clientY + parseInt(offset[1],10)) + 'px');
-    event.preventDefault();
+		// console.log(event.target.attr("class"));
+	    e.dataTransfer.setData("text/plain",
+	    (parseInt($(event.target).css("right"), 10) + e.clientX) + ',' + (parseInt($(event.target).css("bottom"),10) + e.clientY));
+	    console.log(e.dataTransfer.getData("text/plain"));
 	});
 
 	$("body").on("dragover", function(event) {
-		event.preventDefault();
+		event.preventDefault();  
+	});
+
+	$("body").on("drop", function(event) {
+		var e = event.originalEvent;
+		var offset = e.dataTransfer.getData("text/plain").split(',');
+		console.log(offset);
+		console.log(e.clientX + " " + e.clientY);
+	    // $(".toolbar").css("right", (parseInt(offset[0],10) - e.clientX) + 'px');
+	    $(".toolbar").css("right", '0px');
+	    $(".toolbar").css("bottom", (parseInt(offset[1],10) - e.clientY) + 'px');
+		event.preventDefault();  
 	});
 
 });
